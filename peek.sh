561610317b1520bff57530ef90c385bd9f31bba2
AnaMimic <mimicana4@gmail.com>
2023-11-01 21:23:04 +0100

    Day 9 - Homework 1

--- peek.sh
+++ peek.sh
@@ -1,3 +1,21 @@
-head -n $2 $1
-echo ...
-tail -n $2 $1
+FILE=$1
+LINE_COUNT=$2
+
+if [[ -z $LINE_COUNT ]]; then
+	LINE_COUNT=3
+fi
+
+FILE_LINE_COUNT=$(wc -l < $FILE)
+TRESHOLD=$((2 * $LINE_COUNT))
+
+if [[ $FILE_LINE_COUNT -le $TRESHOLD ]]; then
+	# I think the warning should be here saying that the file is smaller than the treshold
+	# echo "The file $FILE is smaller than the threshold of $THRESHOLD"
+	cat $FILE
+else
+	echo "The file $FILE has more lines than $TRESHOLD"
+	head -n $LINE_COUNT $FILE
+	echo ...
+	tail -n $LINE_COUNT $FILE
+fi
+
