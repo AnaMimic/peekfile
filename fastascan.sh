28c2ea6d45ce49328f6c7ba3f59f649af5952c77
AnaMimic <mimicana4@gmail.com>
2023-11-22 18:46:00 +0100

    Symbolic link + for in loop

--- fastascan.sh
+++ fastascan.sh
@@ -17,3 +17,19 @@ all_fa_fasta_files=$(find $X -type f -name "*.fasta" -or -name "*fa")
 
 # how many such files there are
 echo There are: $(echo $all_fa_fasta_files | wc -w) fa/fasta files in $X folder and its subfolders
+
+for file in $all_fa_fasta_files; do
+	echo === File $file report ===
+	unique_fasta_ids_count=$(grep ">" $file | awk '{print $1}' | sort | uniq -c | wc -l)
+	echo File $file has $unique_fasta_ids_count unique fasta IDs
+	
+	if [[ -h $file ]]; then 
+		echo File $file is a symbolic link
+	else
+		echo File $file is not a symbolic link
+	fi
+	
+	num_of_sequences=$(grep ">" $file | wc -l)
+	echo File $file contains $num_of_sequences sequences inside.  
+done
+
